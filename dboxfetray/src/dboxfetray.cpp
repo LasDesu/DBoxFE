053016da58a0fd4b65e4aae36dbc1eb03bd8c788
chmaster <chmaster@ab66aab8-02df-0310-b6be-8546509eb979>
2007-09-09 13:28:26 +0000

    * FIXED		problem with reload of system tray icon menu

--- dboxfetray/src/dboxfetray.cpp
+++ dboxfetray/src/dboxfetray.cpp
@@ -25,15 +25,23 @@
 
 DBoxFE_TrayIcon::DBoxFE_TrayIcon()
 {
-    createMenu();
+	dbfe_trayIconMenu = new QMenu( this );	
+	dbfe_trayIcon = new QSystemTrayIcon( this );
 
-	setWindowIcon(QIcon( QString::fromUtf8( ":/dboxfetray.png") ) );
+	createMenu();
+
+	dbfe_trayIcon->setContextMenu( dbfe_trayIconMenu );
+	dbfe_trayIcon->setIcon( QIcon( QString::fromUtf8( ":/dboxfetray.png" ) ) );
+	dbfe_trayIcon->setToolTip( "DBoxFE - TrayIcon v0.1.3 (SVN)" );
+	dbfe_trayIcon->show();
+
+	setWindowIcon( QIcon( QString::fromUtf8( ":/dboxfetray.png" ) ) );
 	setWindowTitle( getAppTitel() );
 
 	update = new QTimer( this );
 	connect( update, SIGNAL( timeout() ), this, SLOT( reloadMenu() ) );
 	update->thread()->setPriority( QThread::NormalPriority );
-	update->start( 30000 );
+	update->start( 15000 );
 }
 
 void DBoxFE_TrayIcon::setVisible( bool visible )
@@ -50,25 +58,26 @@ void DBoxFE_TrayIcon::closeEvent( QCloseEvent *e )
 
 void DBoxFE_TrayIcon::createMenu()
 {
-	dbfe_trayIconMenu = new QMenu( this );
+	
 	QAction *infoAction = new QAction( getAppTitel(), this );
 	infoAction->setEnabled( FALSE );
+
 	dbfe_trayIconMenu->addAction( infoAction );
 	dbfe_trayIconMenu->addSeparator();
 
-    m_file = QDir::homePath();
-    m_file.append( "/.dboxfe/profile/profile.xml" );
+	m_file = QDir::homePath();
+	m_file.append( "/.dboxfe/profile/profile.xml" );
 
-    XMLPreferences settGP( "DBoxFE", "Alexander Saal" );
-    settGP.setVersion( getAppVersion() );
-    settGP.load( m_file );
+	XMLPreferences settGP( "DBoxFE", "Alexander Saal" );
+	settGP.setVersion( getAppVersion() );
+	settGP.load( m_file );
 
 	/* Get the preferences from dboxfe profile */
-    QStringList sList = settGP.getStringList( "Profile", "Name" );
+	QStringList sList = settGP.getStringList( "Profile", "Name" );
 	dbox_bin = settGP.getString( "DOSBox", "binary" );
 
 	/* Add dynamical menu entries of gameprofiles */
-	for( int i = 0; i < sList.size(); i++ ) {
+	for ( int i = 0; i < sList.size(); i++ ) {
 		qApp->processEvents();
 		dbfe_trayAction = new QAction( sList.value( i ), this );
 		connect( dbfe_trayAction, SIGNAL( triggered() ), this, SLOT( startGame() ) );
@@ -78,31 +87,27 @@ void DBoxFE_TrayIcon::createMenu()
 	/* Create action after profile menus */
 	dbfe_trayIconMenu->addSeparator();
 
-    QAction *dboxfeAction = new QAction( "&DBoxFE", this );
-    connect( dboxfeAction, SIGNAL( triggered() ), this, SLOT( startdboxfe() ) );
+	QAction *dboxfeAction = new QAction( "&DBoxFE", this );
 
-    QAction *quitAction = new QAction( tr("&Quit"), this );
-    connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+	connect( dboxfeAction, SIGNAL( triggered() ), this, SLOT( startdboxfe() ) );
+
+	QAction *quitAction = new QAction( tr( "&Quit" ), this );
+
+	connect( quitAction, SIGNAL( triggered() ), qApp, SLOT( quit() ) );
 
 	/* Create trayicon now and set the context menu */
 	dbfe_trayIconMenu->addAction( dboxfeAction );
 	dbfe_trayIconMenu->addAction( quitAction );
-
-	dbfe_trayIcon = new QSystemTrayIcon( this );
-	dbfe_trayIcon->setContextMenu( dbfe_trayIconMenu );
-
-	dbfe_trayIcon->setIcon( QIcon( QString::fromUtf8( ":/dboxfetray.png" ) ) );
-	dbfe_trayIcon->setToolTip("DBoxFE - TrayIcon v0.1.2 (SVN)");
-	dbfe_trayIcon->show();
 }
 
 void DBoxFE_TrayIcon::reloadMenu()
 {
 	update->stop();
-	delete dbfe_trayIconMenu;
-	delete dbfe_trayIcon;
+
+	dbfe_trayIconMenu->clear();
 	createMenu();
-	update->start(15000);
+
+	update->start( 10000 );
 }
 
 void DBoxFE_TrayIcon::startGame()
@@ -110,7 +115,7 @@ void DBoxFE_TrayIcon::startGame()
 	QAction *action = qobject_cast<QAction *>( sender() );
 	QString gProfile = QDir::homePath() + "/.dboxfe/" + action->text() + ".conf";
 
-	if( dbox_bin.isEmpty() || dbox_bin.isNull() ) {
+	if ( dbox_bin.isEmpty() || dbox_bin.isNull() ) {
 		QMessageBox::information( this, getAppTitel(), tr( "Can not start dosbox, no dosbox binary was selected.\nPlease choose dosbox binary." ) );
 		return;
 	}
@@ -120,36 +125,36 @@ void DBoxFE_TrayIcon::startGame()
 
 void DBoxFE_TrayIcon::start( const QString& bin, const QString &param, const QString &conf )
 {
-
-    dBox = new QProcess( this );
-
-#ifdef Q_OS_WIN32
-    m_param.append( "-noconsole" );
-#endif
-
-    m_param.append( param );
-    m_param.append( conf );
+	dBox = new QProcess( this );
 
 #ifdef Q_OS_WIN32
-    QFileInfo dboxBin( conf );
-    dBox->setWorkingDirectory( dboxBin.absolutePath() );
+	m_param.append( "-noconsole" );
 #endif
 
-    dBox->start( bin, m_param );
+	m_param.append( param );
+	m_param.append( conf );
+
+#ifdef Q_OS_WIN32
+	QFileInfo dboxBin( conf );
+	dBox->setWorkingDirectory( dboxBin.absolutePath() );
+#endif
+
+	dBox->start( bin, m_param );
 
 	m_param.clear();
 }
 
 void DBoxFE_TrayIcon::startdboxfe()
+
 {
-	 QString dbfeBin;
-	 dboxfe = new QProcess( this );
+	QString dbfeBin;
+	dboxfe = new QProcess( this );
 
-	 #ifdef Q_OS_WIN32
-	 	dbfeBin = QCoreApplication::applicationDirPath() + "/dboxfe.exe";
-	 #else
-	 	dbfeBin = QCoreApplication::applicationDirPath() + "/dboxfe";
-	 #endif
+#ifdef Q_OS_WIN32
+	dbfeBin = QCoreApplication::applicationDirPath() + "/dboxfe.exe";
+#else
+	dbfeBin = QCoreApplication::applicationDirPath() + "/dboxfe";
+#endif
 
-	 dboxfe->start( dbfeBin, QStringList());
+	dboxfe->start( dbfeBin, QStringList() );
 }
